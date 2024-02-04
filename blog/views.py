6b15dac6c9227e78d4ad5f4ea6cb18788c599dce
okiromosh <moses.okiror93@gmail.com>
2024-02-04 14:03:21 +0300

    added create template

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'index.html')
+
+def create(request):
+    context = {}
+    return render(request, 'create.html')
