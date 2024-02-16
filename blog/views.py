7807e77627cc81c133f6c661c0e473823a10f93e
okiromosh <moses.okiror93@gmail.com>
2024-02-16 15:05:06 +0300

    update create view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import *
 from .forms import PostForm
@@ -47,11 +48,14 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+@login_required(login_url='login')
 def create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save()
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
             return redirect('index', )  # Change 'post_detail' to your actual detail view name
     else:
         form = PostForm()
